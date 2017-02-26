c301b89351860c993ef742c2a8feb13fc6430fa3
yadavmukesh <yadav92mukesh@gmail.com>
2017-02-26 15:42:55 +0530

    Update negwords.m

--- negwords.m
+++ negwords.m
@@ -1,14 +1,14 @@
+%% To save list of negation words in Hindi 
 clc;
 clear all;
 close all;
 
 feature('DefaultCharacterSet', 'UTF8');
-[fid,msg]=fopen('negwords.txt','r','n','UTF-8');
+[fid,msg]=fopen('negwords.txt','r','n','UTF-8');  %% open files having prestored negation words
 negword = fscanf(fid,'%c');
-negword = regexp(negword, '[,]', 'split')
+negword = regexp(negword, '[,]', 'split')  %% split the negation words using comma, as they are stored in such a way
 fclose(fid);
 
 %snegword = regexp(negword, '[,]', 'split');
 
-save negword;
-
+save negword;  %% contains negation words in Hindi
